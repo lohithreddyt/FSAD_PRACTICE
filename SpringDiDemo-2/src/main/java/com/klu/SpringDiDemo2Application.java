e1debdbbd5d017f05182c4f0160f39407c75a0c1
lohithreddyt <lohithreddy364@gmail.com>
2026-01-22 17:17:06 +0530

    six file

---
+++ SpringDiDemo-2/src/main/java/com/klu/SpringDiDemo2Application.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiDemo2Application.class, args);
+	}
+
+}
